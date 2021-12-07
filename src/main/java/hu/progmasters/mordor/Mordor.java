1c90dd4aad40223a8ce198c0afe3a4b6bcb36a69
GaborOrban86 <86094931+GaborOrban86@users.noreply.github.com>
2021-12-07 08:41:04 +0100

    Mordor letrehozva.

--- src/main/java/hu/progmasters/mordor/Mordor.java
+++ src/main/java/hu/progmasters/mordor/Mordor.java
@@ -1,5 +1,41 @@
 package hu.progmasters.mordor;
 
-public class Mordor {
+import hu.progmasters.kozosAbstractok.HarciEgysegek;
+import hu.progmasters.mordor.ostromgepek.Ballista;
+import hu.progmasters.mordor.ostromgepek.Katapult;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mordor extends HarciEgysegek {
+    private HarciEgysegek harciEgysegek;
+    private List<Katapult> katapultList = new ArrayList<>();
+    private List<Ballista> ballistaList = new ArrayList<>();
+
+    public Mordor() {
+    }
+
+    public HarciEgysegek getHarciEgysegek() {
+        return harciEgysegek;
+    }
+
+    public void setHarciEgysegek(HarciEgysegek harciEgysegek) {
+        this.harciEgysegek = harciEgysegek;
+    }
+
+    public List<Katapult> getKatapultList() {
+        return katapultList;
+    }
+
+    public void setKatapultList(List<Katapult> katapultList) {
+        this.katapultList = katapultList;
+    }
+
+    public List<Ballista> getBallistaList() {
+        return ballistaList;
+    }
+
+    public void setBallistaList(List<Ballista> ballistaList) {
+        this.ballistaList = ballistaList;
+    }
 }
